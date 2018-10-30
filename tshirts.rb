b84d6fe41d4a93d746f2df6935745ad15fd9a95d
berco <bercob@gmail.com>
2018-10-30 16:56:22 +0100

    solve tshirts - WIP

--- tshirts.rb
+++ tshirts.rb
@@ -1,20 +1,38 @@
-#!/usr/bin/ruby
+#!/home/berco/.rvm/rubies/ruby-2.3.1/bin/ruby
 
 quest = File.open 'tshirts_1.txt'
 tshirts_count, days_count = quest.gets.split(' ').map {|number| number.to_i}
 tshirts = quest.gets.split(' ').map {|number| number.to_i}
 
 if tshirts_count != tshirts.length
-	p 'error'
-	exit 1
+  p 'error'
+  exit 1
 end
 
 definers = Array.new
+used_tshirts = Array.new
 
 days_count.times do
-	definers.push quest.gets.strip
+  definers.push quest.gets.strip
 end
 
+p tshirts
+
 definers.each do |definer|
-	p definer
+  if definer == 'Z'
+    tshirts[0] += 1
+    used_tshirts.push 1
+  elsif definer == 'L'
+    min_number = tshirts.min
+    min_index = tshirts.index min_number
+    tshirts.delete_at min_index
+    tshirts.unshift min_number + 1
+    used_tshirts.push min_index + 1
+  else
+    p 'error: unknown definer'
+    exit 2
+  end
 end
+
+p tshirts
+p used_tshirts
